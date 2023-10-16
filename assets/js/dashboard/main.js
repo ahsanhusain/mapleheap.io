9d5bdcdb4aad9c111f89c3bdf010417ba25a1860
Sameer Khan <sameer.qsols@gmail.com>
2023-10-17 01:40:44 +0500

    Form Design

--- assets/js/dashboard/main.js
+++ assets/js/dashboard/main.js
@@ -16,15 +16,14 @@ $(document).ready(function() {
             },
             "emptyTable": "No data available in table"
         },
-        "scrollX": true, // Enable horizontal scrolling
+        "scrollX": true,
         "columnDefs": [
             {
-                "targets": "_all", // Apply to all columns
-                "className": "dt-nowrap" // Prevent text wrapping
+                "targets": "_all",
+                "className": "dt-nowrap"
             }
         ]
     });
-    // Dynamically add the "Add Title" button to the search input
     var $searchInput = $('div.dataTables_filter input');
     $searchInput.attr("placeholder", "Search");
     var $addTitleButton = $('<button id="add-title-button" class="tableBtn">Add Title <i class="fa fa-plus"></i></button>');
@@ -32,8 +31,8 @@ $(document).ready(function() {
     $searchInput.after($addTitleButton);
 
     // Add the search icon to the input field
-    $searchInput.css('background-image', 'url("assets/images/dashboard/search.png")'); // Replace with the path to your search icon image
-    $searchInput.css('background-position', '98% 50%'); // Adjust the position as needed
+    $searchInput.css('background-image', 'url("assets/images/dashboard/search.png")');
+    $searchInput.css('background-position', '98% 50%');
     $searchInput.css('background-repeat', 'no-repeat');
     $searchInput.css('padding', '0.375rem 0.75rem');
     $searchInput.css('line-height', '1.8');
@@ -44,9 +43,6 @@ $(document).ready(function() {
     $('#example td').css('padding', '10px');
     $('#example td').css('color', '#4B4B4D');
 
-    table.on('order.dt', function () {
-        // table.cells().nodes().to$().removeClass('sorting_1 even odd');
-    });
     table.cells().nodes().to$().addClass('custom-bg-color');
     function updatePagination() {
         var paginationControls = $('.dataTables_paginate').clone();
@@ -72,10 +68,8 @@ $(document).ready(function() {
         });
     }
     
-    // Attach click event handlers for custom pagination controls
     attachCustomPaginationHandlers();
 });
 $(document).on('click', '#add-title-button', function() {
-    // Add your code to handle the "Add Title" button click event here.
     alert('Add Title button clicked');
 });
